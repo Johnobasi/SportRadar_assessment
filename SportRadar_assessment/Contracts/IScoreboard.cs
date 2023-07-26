c6cdfdb56f8c606af196c7a2b16e3a0208000524
John Inyang <johni@atlashxm.com>
2023-07-26 10:44:17 +0100

    Add project files.

---
+++ SportRadar_assessment/Contracts/IScoreboard.cs
@@ -0,0 +1,10 @@
+﻿namespace SportRadar_assessment.Contracts
+{
+    public interface IScoreboard
+    {
+        void StartMatch(string homeTeam, string awayTeam);
+        void UpdateScore(string homeTeam, string awayTeam, int homeScore, int awayScore);
+        void FinishMatch(string homeTeam, string awayTeam);
+        List<string> GetSummary();
+    }
+}
---
+++ SportRadar_assessment/Services/Scoreboard.cs
@@ -0,0 +1,27 @@
+﻿using SportRadar_assessment.Contracts;
+
+namespace SportRadar_assessment.Services
+{
+    public class Scoreboard : IScoreboard
+    {
+        public void FinishMatch(string homeTeam, string awayTeam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetSummary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartMatch(string homeTeam, string awayTeam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateScore(string homeTeam, string awayTeam, int homeScore, int awayScore)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
